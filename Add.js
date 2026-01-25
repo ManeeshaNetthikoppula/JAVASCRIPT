040d126f27baa954fa578116f1eb8436b163c11d
ManeeshaNetthikoppula <netthikoppulamaneesha@gmail.com>
2026-01-25 11:38:58 +0530

    updated file

--- Add.js
+++ Add.js
@@ -1,4 +1,15 @@
-let num1 = 5;
+/*let num1 = 5;
 let num2 = 7;
 let sum = num1 + num2;
-console.log("sum :", sum);
+console.log("sum :", sum);*/
+
+//using function
+/*Syntax =>function additionFunction(a,b){
+    return  a + b;
+}*/
+//Using arrow function
+//let addition = (a,b) =>a + b;
+let num1 = 5;
+let num2 = 7;
+let sum = addition(num1,num2);
+console.log("Sum of given numbers is: ",sum);
