d86c6e41775a1bbad1303c895f7e732f83b5eb5a
ManeeshaNetthikoppula <netthikoppulamaneesha@gmail.com>
2026-01-25 12:08:38 +0530

    updated

--- Basic.js
+++ Basic.js
@@ -1,5 +1,5 @@
 //Operators in JavaScript=>Arithmetic(+,-,*,/,%),
-// //Adding Two Nums
+// //Adding Two Nums(+)
 /*let num1 = 5;
 let num2 = 7;
 let sum = num1 + num2;
@@ -15,9 +15,25 @@ console.log("sum :", sum);*/
 let num2 = 7;
 let sum = addition(num1,num2);
 console.log("Sum of given numbers is: ",sum);*/
+//Substraction(-)
+/*let num1 = 10;
+let num2 = 1;
+let result = num1 - num2;
+console.log("Result: ",result);*/
 
-//Multiplication of two numbers
-let num1 = 5;
+
+//Multiplication of two numbers(*)
+/*let num1 = 5;
 let num2 = 6;
 let result = num1*num2;
-console.log("Result :",result);
+console.log("Result :",result);*/
+//Division(/)
+/*let num1 = 20;
+let num2 = 30;
+let result = num1 / num2;
+console.log("Division:", result);*/
+//percentile(%)
+/*let num1 = 100;
+let num2 = 10;
+let result = (10 % 100);
+console.log("percentile:",result);*/
