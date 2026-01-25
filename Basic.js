760a228e603ee3d4d8baa0daed3cb41a0e6bbfc0
ManeeshaNetthikoppula <netthikoppulamaneesha@gmail.com>
2026-01-25 11:48:51 +0530

    updated

--- Basic.js
+++ Basic.js
@@ -1,3 +1,5 @@
+//Operators in JavaScript=>Arithmetic(+,-,*,/,%),
+// //Adding Two Nums
 /*let num1 = 5;
 let num2 = 7;
 let sum = num1 + num2;
@@ -9,7 +11,13 @@ console.log("sum :", sum);*/
 }*/
 //Using arrow function
 //let addition = (a,b) =>a + b;
-let num1 = 5;
+/*let num1 = 5;
 let num2 = 7;
 let sum = addition(num1,num2);
-console.log("Sum of given numbers is: ",sum);
+console.log("Sum of given numbers is: ",sum);*/
+
+//Multiplication of two numbers
+let num1 = 5;
+let num2 = 6;
+let result = num1*num2;
+console.log("Result :",result);
