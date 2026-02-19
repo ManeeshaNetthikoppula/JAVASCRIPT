2bcc41a238712d386ddfb19f6ca7ede0721700da
ManeeshaNetthikoppula <netthikoppulamaneesha@gmail.com>
2026-02-19 10:01:48 +0530

    uploads

--- Basic.js
+++ Basic.js
@@ -614,6 +614,7 @@ class EventEmitter{
     }
 }*/
 //Throttle function
+/*
 function throttle(fn, limit) {
   let inThrottle = false;
 
@@ -627,4 +628,18 @@ function throttle(fn, limit) {
       }, limit);
     }
   };
+}*/
+// Detect cycle in linkedlist
+function hasCycle(head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) return true;
+  }
+
+  return false;
 }
