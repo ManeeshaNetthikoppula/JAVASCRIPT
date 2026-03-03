5c96ad990ddcb1c7a0e704f3b1fad2b6f84e8256
ManeeshaNetthikoppula <netthikoppulamaneesha@gmail.com>
2026-03-03 11:36:41 +0530

    uploads

--- ReactDeveloperLevel.js
+++ ReactDeveloperLevel.js
@@ -11,11 +11,36 @@ function useDebounce(value,delay){
     return debouncedValue;
 }*/
 // 2. Build a custom usePrevious Hook
-import { useEffect, useRef } from "react";
+/*import { useEffect, useRef } from "react";
 function usePrevious(value){
     const ref = useRef();
     useEffect(() => {
         ref.current = value;
     }, [ value]);
     return ref.current;
-}
+}*/
+// 3. Infinite Scroll Component
+import { useEffect, useState } from "react";
+function InfiniteScroll(){
+    cont [DataTransferItemList, setItems] = useState([]);
+    const[ page, setPage]= useState(1);
+    useEffect(() =>{
+        fetch('https: //jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}')
+        .then(res => res.json())
+        .then(data => setItems(prev => [...prev, ...data]));}, [page]);
+        useEffect(() => {
+            function handleScroll(){
+                if(window.innerHeight + window.scrollY >= document.body.offsetHeight){
+                    setPage(prev => prev + 1);
+                }
+            }
+            window.addEventListener("scroll", handleScroll);
+            return()=>
+                window.removeEventListener("scroll", handleScroll);
+        }, []);
+        return (
+            <div>
+                {items.map(items => <p key={items.id}>{items.title}</p>)}
+            </div>
+        )
+    }
